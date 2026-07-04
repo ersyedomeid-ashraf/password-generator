dde9df515ada23f3fdb54af266c5b1f025a04166
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-04 11:15:31 +0530

    Developed password generation algorithm

--- secure_password_generator.py
+++ secure_password_generator.py
@@ -101,7 +101,7 @@ letters = [
     "Y",
     "Z",
 ]
-numers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 symbols = ["!", "@", "#", "$", "%", "&", "(", ")", "*", "+"]
 
 print("Welcome to Password Generator! ")
@@ -111,3 +111,15 @@ n_numbers = int(input("How many numbers you want in your password?\n"))
 n_symbols = int(input("How manny symbols you want in your password?\n"))
 
 password_list = []
+
+for i in range(1, n_letters + 1):
+    char = random.choice(letters)
+    password_list += char
+
+for i in range(1, n_numbers + 1):
+    char = random.choice(numbers)
+    password_list += char
+
+for i in range(1, n_symbols + 1):
+    char = random.choice(symbols)
+    password_list += char
