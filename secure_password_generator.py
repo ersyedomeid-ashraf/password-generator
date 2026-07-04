906ccb608e375a0768bc93c96dfb26e092e62af8
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-07-04 08:04:26 +0530

    Added setup and user input workflow

--- secure_password_generator.py
+++ secure_password_generator.py
@@ -1,4 +1,20 @@
 """
 PASSWORD GENERATOR - WORKFLOW
 
+1- Import Required Module
+- Import random module.
+
+2- Create Character Lists
+- Create letters list.
+- Create numbers lists.
+- Create symbols lists.
+
+3- Display Welcome Message
+- Print welcome message.
+
+4- Input from User
+- Enter number of letters.
+- Enter number of numbers.
+- Enter number of symbols.
+
 """
